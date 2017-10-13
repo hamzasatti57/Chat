ecf26a9db5e3cac56f35061dd1f31e3ea5145b4a
Hamza Satti <hamza.satti159@gmail.com>
2017-10-13 18:27:09 +0500

    Some helpful message for your future self

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011102338) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20171013064352) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "recipient_id"
     t.integer  "sender_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true, using: :btree
-    t.index ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
-    t.index ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
+    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -31,8 +28,13 @@ ActiveRecord::Schema.define(version: 20171011102338) do
     t.integer  "conversation_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,10 +56,8 @@ ActiveRecord::Schema.define(version: 20171011102338) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
 end
